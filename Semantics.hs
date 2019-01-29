47395415f1f44596240245f6c25f86938f11ea95
Richard Cook <rcook@rcook.org>
2019-01-28 20:01:59 -0800

    Extract main from Semantics.hs

--- Semantics.hs
+++ Semantics.hs
@@ -1,8 +1,5 @@
-#!/usr/bin/env stack
--- stack --resolver=lts-12.6 script
-
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# OPTIONS_GHC -Wincomplete-patterns #-}
+{-# OPTIONS_GHC -Wall #-}
 
 module Semantics
     ( Expr(..)
@@ -15,7 +12,6 @@ module Semantics
     , evaluate
     , extend
     , lookup
-    , main
     , not
     ) where
 
@@ -80,37 +76,3 @@ evaluate (And f1 f2) i = and <$> evaluate f1 i <*> evaluate f2 i
 evaluate (Or f1 f2) i = or <$> evaluate f1 i <*> evaluate f2 i
 evaluate (Implies f1 f2) i = or <$> (not <$> evaluate f1 i) <*> evaluate f2 i
 evaluate (Equiv f1 f2) i = equals <$> evaluate f1 i <*> evaluate f2 i
-
-main :: IO ()
-main = do
-    let i0 = assign [(Name "x0", ValueFalse), (Name "x1", ValueFalse)]
-    --print i0
-    --print $ lookup (Name "x0") i0
-    let i1 = extend (Name "x2") ValueTrue i0
-    --print i1
-    --print $ evaluate ExprTrue i1
-    --print $ evaluate (ExprVar (Name "x0")) i1
-    --print $ evaluate (ExprNot (ExprVar (Name "x0"))) i1
-    putStrLn "main"
-    print $ evaluate (Implies (Var (Name "x0")) (Var (Name "x1"))) i1
-
-    example1
-    example2
-
-example1 :: IO ()
-example1 = do
-    let x1 = Name "x1"
-        x2 = Name "x2"
-        false = ValueFalse
-        true = ValueTrue
-    putStrLn "example1"
-    print $ evaluate (And (Not (Var x1)) (Var x2)) (assign [(x1, false), (x2, true)])
-
-example2 :: IO ()
-example2 = do
-    let x1 = Name "x1"
-        x2 = Name "x2"
-        false = ValueFalse
-        true = ValueTrue
-    putStrLn "example2"
-    print $ evaluate (And (Not (Var x1)) (Var x2)) (assign [(x1, true), (x2, true)])
---
+++ SemanticsDemo.hs
@@ -0,0 +1,45 @@
+#!/usr/bin/env stack
+{- stack --resolver=lts-12.6 script
+    --package containers
+-}
+
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -Wall #-}
+
+module SemanticsDemo (main) where
+
+import SATPrelude
+import Semantics
+
+main :: IO ()
+main = do
+    let i0 = assign [(Name "x0", ValueFalse), (Name "x1", ValueFalse)]
+    --print i0
+    --print $ lookup (Name "x0") i0
+    let i1 = extend (Name "x2") ValueTrue i0
+    --print i1
+    --print $ evaluate ExprTrue i1
+    --print $ evaluate (ExprVar (Name "x0")) i1
+    --print $ evaluate (ExprNot (ExprVar (Name "x0"))) i1
+    putStrLn "main"
+    print $ evaluate (Implies (Var (Name "x0")) (Var (Name "x1"))) i1
+
+    example1
+    example2
+
+example1 :: IO ()
+example1 = do
+    let x1 = Name "x1"
+        x2 = Name "x2"
+        false = ValueFalse
+        true = ValueTrue
+    putStrLn "example1"
+    print $ evaluate (And (Not (Var x1)) (Var x2)) (assign [(x1, false), (x2, true)])
+
+example2 :: IO ()
+example2 = do
+    let x1 = Name "x1"
+        x2 = Name "x2"
+        true = ValueTrue
+    putStrLn "example2"
+    print $ evaluate (And (Not (Var x1)) (Var x2)) (assign [(x1, true), (x2, true)])
