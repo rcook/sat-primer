45bc8f185682ae7cc2d93aafb9f3795fa103b567
Richard Cook <rcook@rcook.org>
2019-01-30 21:47:01 -0800

    Use breakOnJust

--- ValidityDeduce.hs
+++ ValidityDeduce.hs
@@ -86,3 +86,44 @@ deduceValid expr = case runState (allClosed [IFalsifies expr]) (Deduction [] [])
                 rule (ISatisfies (Equiv f1 f2)) = Just . branch [ISatisfies (And f1 f2), IFalsifies (Or f1 f2)]
                 rule (IFalsifies (Equiv f1 f2)) = Just . branch [ISatisfies (And f1 (Not f2)), ISatisfies (And (Not f1) f2)]
                 rule _ = const Nothing
+
+-- | 'breakOnJust', applied to a function @f@ and a list @xs@, returns a triple
+-- where the first element is the prefix (possibly empty) of @xs@ of elements
+-- for which the function @f@ evaluates to @Nothing@, the second element is the
+-- result of applying @f@ to the first element in @xs@ for which it evaluates to
+-- non-@Nothing@ and the third element is the (possibly empty) remainder of the
+-- list.
+breakOnJust ::
+    (a -> Maybe b)          -- ^ function @f@
+    -> [a]                  -- ^ list @xs@
+    -> ([a], Maybe b, [a])  -- ^ result
+breakOnJust _ [] = ([], Nothing, [])
+breakOnJust f (x : xs) =
+    case f x of
+        result@(Just _) -> ([], result, xs)
+        _ -> let (gs, result, hs) = breakOnJust f xs in (x : gs, result, hs)
+
+data Conclusion = CAnd [Fact] | CBranch [Fact]
+
+prule :: Fact -> Maybe Conclusion
+prule (ISatisfies (Not f)) = Just $ CAnd [IFalsifies f]
+prule (IFalsifies (Not f)) = Just  $ CAnd [ISatisfies f]
+prule (ISatisfies (And f1 f2)) = Just $ CAnd [ISatisfies f1, ISatisfies f2]
+prule (IFalsifies (And f1 f2)) = Just $ CBranch [IFalsifies f1, IFalsifies f2]
+prule (ISatisfies (Or f1 f2)) = Just $ CBranch [ISatisfies f1, ISatisfies f2]
+prule (IFalsifies (Or f1 f2)) = Just $ CAnd [IFalsifies f1, IFalsifies f2]
+prule (ISatisfies (Implies f1 f2)) = Just $ CBranch [IFalsifies f1, ISatisfies f2]
+prule (IFalsifies (Implies f1 f2)) = Just $ CAnd [ISatisfies f1, IFalsifies f2]
+prule (ISatisfies (Equiv f1 f2)) = Just $ CBranch [ISatisfies (And f1 f2), IFalsifies (Or f1 f2)]
+prule (IFalsifies (Equiv f1 f2)) = Just $ CBranch [ISatisfies (And f1 (Not f2)), ISatisfies (And (Not f1) f2)]
+prule _ = Nothing
+
+allClosed' :: [Fact] -> Bool
+allClosed' fs =
+    case breakOnJust prule fs of
+        (gs, Just (CAnd fs'), hs) -> allClosed' (fs' ++ gs ++ hs)
+        (gs, Just (CBranch fs'), hs) -> all (== True) (map (\f -> allClosed' (f : gs ++ hs)) fs')
+        (gs, Nothing, hs) ->
+            case findContradictions fs of
+                [] -> False
+                _ -> True
