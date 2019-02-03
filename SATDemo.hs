630240a5ee4473e957ae9aaec44d9cfe98b7f415
Richard Cook <rcook@rcook.org>
2019-02-02 20:21:57 -0800

    Week 1 example 2

--- SATDemo.hs
+++ SATDemo.hs
@@ -22,8 +22,15 @@ var = Var . Name
 main :: IO ()
 main = hspec $ do
     describe "examples from https://www.coursera.org/learn/automated-reasoning-sat week 1" $ do
-        it "solves example 1" $ do
+        it "finds example 1 satisfiable" $ do
             let p = var "p"
                 q = var "q"
-            searchSat (((p `Or` q) `And` (Not p `Or` Not q)) `And` (p `Or` Not q))
-                `shouldBe` Just True
+                expr = (((p `Or` q) `And` (Not p `Or` Not q)) `And` (p `Or` Not q))
+            expr `shouldBe` And (And (Or (Var (Name "p")) (Var (Name "q"))) (Or (Not (Var (Name "p"))) (Not (Var (Name "q"))))) (Or (Var (Name "p")) (Not (Var (Name "q"))))
+            searchSat expr `shouldBe` Just True
+        it "finds example 2 unsatisfiable" $ do
+            let p = var "p"
+                q = var "q"
+                expr = ((((p `Or` q) `And` (Not p `Or` Not q)) `And` (Not p `Or` q)) `And` (p `Or` Not q))
+            expr `shouldBe` And (And (And (Or (Var (Name "p")) (Var (Name "q"))) (Or (Not (Var (Name "p"))) (Not (Var (Name "q"))))) (Or (Not (Var (Name "p"))) (Var (Name "q")))) (Or (Var (Name "p")) (Not (Var (Name "q"))))
+            searchSat expr `shouldBe` Just False
